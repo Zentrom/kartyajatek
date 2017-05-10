0ebcee8cc5c35b905ba7d92964c2428e1e6f00df
Lipcsei <Lipcsei@192.168.102.202>
2017-05-11 00:49:32 +0200

    passziansz start

--- src/main/java/edu/elte/ik/preszk/cardgame/casualGames/Passziansz.java
+++ src/main/java/edu/elte/ik/preszk/cardgame/casualGames/Passziansz.java
@@ -1,8 +1,19 @@
 package edu.elte.ik.preszk.cardgame.casualGames;
 
+import edu.elte.ik.preszk.cardgame.Deck;
 import java.io.*;
 import java.util.*;
 
 public class Passziansz{
 
-}
+    public void game(){
+        while(true){
+            boolean francia = false;
+            Deck pakli = new Deck(francia);
+            
+            pakli.printDeck();
+            
+            break;
+        }
+    }
+}   
